fc6ac29dce3c374a1c5e677acc55d8638c65e928
Ataf_Georges <ataf_eleid@hotmail.com>
2023-12-06 16:18:53 +0100

    third commit

--- index.php
+++ index.php
@@ -96,29 +96,106 @@
 // echo "Il a été saisi à la position : $positionPlusGrand\n";
 // EXERCICES 9
 
-$plusGrand = 0;
-$positionPlusGrand = 0;
-$compteur =0;
+// $plusGrand = 0;
+// $positionPlusGrand = 0;
+// $compteur =0;
 
-do {
-    $nombre = readline("Saisissez un nombre (saisie s'arrête à 0) : ");
-    // Vérifier si la saisie est différente de zéro
-    if($nombre !=0){
-        $compteur ++;
-        if($nombre > $plusGrand){
-            $plusGrand = $nombre;
-            $positionPlusGrand = $compteur;
-        }
-    }
-} while ($nombre != 0);
-if($plusGrand !== 0){
-    echo "Le plus grand nombre saisi est : $plusGrand";
-    print PHP_EOL;
-    echo "Il a été saisi à la position : $positionPlusGrand";
-} else {
-    echo "Aucun nombre n'a été saisi.";
-    print PHP_EOL;
-}
+// do {
+//     $nombre = readline("Saisissez un nombre (saisie s'arrête à 0) : ");
+//     Vérifier si la saisie est différente de zéro
+//     if($nombre !=0){
+//         $compteur ++;
+//         if($nombre > $plusGrand){
+//             $plusGrand = $nombre;
+//             $positionPlusGrand = $compteur;
+//         }
+//     }
+// } while ($nombre != 0);
+// if($plusGrand !== 0){
+//     echo "Le plus grand nombre saisi est : $plusGrand";
+//     print PHP_EOL;
+//     echo "Il a été saisi à la position : $positionPlusGrand";
+// } else {
+//     echo "Aucun nombre n'a été saisi.";
+//     print PHP_EOL;
+// }
 // EXERCICES 9
+// function calculateTotalMontant($purchases){
+//     return array_sum ($purchases);
+// }
+// function simulateDiscount($totalAmount,$amountPaid){
+//     $change = $amountPaid - $totalAmount;
+//      Array of available bills
+//     $bills = array(10, 5, 1);
+//      Display the change to give
+//     echo "Change to give :$change Euros";
+//     print PHP_EOL;
+
+//     foreach ($bills as $bill ) {
+//         $billCount = intval($change / $bill);
+//         $change %= $bill;
+//         if($billCount > 0){
+//             echo "$bill Euro(s): $billCount bill(s)";
+//             print PHP_EOL;
+//         }
+//     }
+// }
+// echo "Enter the price of purchase (entre 0 to finish) ";
+// print PHP_EOL;
+
+// $purchases = array();
+// do {
+//     $price = intval(readline("Enter th price (in Euros): "));
+//     if($price != 0){
+//         $purchases[]= $price;
+//     }
+// } while ($price != 0);
+// $totalAmount = calculateTotalMontant($purchases);
+// echo "Total amounts owed :$totalAmount Euros";
+// print PHP_EOL;
+// $amountPaid = (int)readline("Enter the amount paid (in Euros) :");
+// echo "Amount paid : $amountPaid Euros";
+// print PHP_EOL;
+// simulateDiscount(array_sum($purchases),$amountPaid );
+// EXERCICES 10
+
+function calculateWinningChances($totalHorses, $playedHorses)
+{
+    // Calculate factorials
+    $factorialTotal = factorial($totalHorses);
+    $factorialPlayed = factorial($playedHorses);
+    $factorialRemaining = factorial($totalHorses - $playedHorses);
+
+    // Calculate winning chances for each position
+    $chancesThird = $factorialTotal / ($factorialRemaining * $factorialPlayed);
+    $chancesQuarter = $factorialTotal / (($factorialPlayed * $factorialRemaining * 4) * $factorialRemaining);
+    $chancesFifth = $factorialTotal / (($factorialPlayed * $factorialRemaining * 5) * $factorialRemaining);
+
+    return [
+        'Third' => $chancesThird,
+        'Quarter' => $chancesQuarter,
+        'Fifth' => $chancesFifth,
+    ];
+}
+
+function factorial($n)
+{
+    if ($n == 0 || $n == 1) {
+        return 1;
+    } else {
+        return $n * factorial($n - 1);
+    }
+}
+
+// Read user input for the number of horses
+$totalHorses = intval(readline("Enter the total number of horses: "));
+$playedHorses = intval(readline("Enter the number of horses played: "));
+
+// Calculate and display winning chances
+$chances = calculateWinningChances($totalHorses, $playedHorses);
+echo "Chances of winning:\n";
+echo "In Third: 1 in {$chances['Third']} chances\n";
+echo "In Quarter: 1 in {$chances['Quarter']} chances\n";
+echo "In Fifth: 1 in {$chances['Fifth']} chances\n";
 
 ?>
